1d3ae1caf4dc33d896a35dadc04894074eaee44f
anaya <anaya@email.com>
2024-09-11 19:34:47 -0600

    funcionamiento terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Parcial1Component } from './parcial-1/parcial-1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Parcial1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/parcial-1/parcial-1.component.ts
+++ src/app/parcial-1/parcial-1.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parcial-1',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './parcial-1.component.html',
   styleUrl: './parcial-1.component.css'
 })
 export class Parcial1Component {
+   color:string = "red";
 
+  ValidarColor(){
+    const exp = /[0-9a-zA-Z]/ig;
+    const expText = /[a-z]/ig;
+
+    if(expText.test(this.color)){
+      this.clearMsgWarning();
+    }
+    else if(exp.test(this.color)){
+      if(!this.color.includes('#')) {
+        this.color = '#' + this.color;
+        this.clearMsgWarning();
+      }
+    }else{
+      this.msgWarning('Color no valido')
+    }
+  }
+
+  msgWarning(msg: string){
+    const help = document.getElementById('colorHelp');
+    if(help) help.textContent = msg;
+  }
+
+  clearMsgWarning(){
+    const help = document.getElementById('colorHelp');
+    if(help) help.textContent = '';
+  }
 }
